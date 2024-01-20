3a6c380c05df3fbf6d17fadd663dd42341fd96a3
Bogdan <b.baloiu@hotmail.com>
2024-01-20 16:57:47 -0500

    change file name

--- ParkME/Frontend/App.js
+++ ParkME/Frontend/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MyCamera from './components/Camera';
+import MyCamera from './components/CameraComponent';
 
 export default function App() {
   
